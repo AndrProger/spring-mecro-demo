ee9451f06226fe2bb32192fb0b47e4933bbd3376
Андрей Андреевич Стариненко <114221320+AndrProger@users.noreply.github.com>
2024-02-22 02:26:30 +0600

    add new microservice

---
+++ eureka-client-2/src/main/java/kz/prog/micro/demo/eurekaclient2/controller/NewController.java
@@ -0,0 +1,15 @@
+package kz.prog.micro.demo.eurekaclient2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/new")
+public class NewController {
+
+  @GetMapping("/name")
+  public String test() {
+    return "Hello world! I am new controller!";
+  }
+}
